c393bc1f19bf163ae04338adf50e1219570f966d
shinbeomsik <tnghrltkwnst@naver.com>
2023-11-29 15:31:37 +0900

    7장 블로그 정리

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -61,4 +61,52 @@ public class ArticleController {
 		model.addAttribute("articleList", articleEntityList);
 		return "articles/index";
 	}
+	
+	
+	@GetMapping("/articles/{id}/edit")
+	public String edit(@PathVariable Long id ,Model model) {
+		//수정할 데이터 가져오기
+		Article articleEntity = articleRepository.findById(id).orElse(null);
+		// 모델에 데이터 등록
+		model.addAttribute("article", articleEntity);
+		// 뷰 페이지 설정
+	    return "articles/edit";  
+	}
+	
+	@PostMapping("/articles/update")
+	public String update(ArticleForm form) {
+		log.info(form.toString());
+	    // 1. DTO를 엔티티로 변환하기
+		Article articleEntity = form.toEntity();
+		log.info(articleEntity.toString());
+	    // 2. 엔티티를 DB에 저장하기
+		Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+		
+		if (target != null) {
+		    articleRepository.save(articleEntity);
+		}
+	    // 3. 수정 결과 페이지로 리다이렉트하기
+		return "redirect:/articles/" + articleEntity.getId();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
