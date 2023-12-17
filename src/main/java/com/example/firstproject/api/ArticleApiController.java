d35e45e511e3542cbf1cb577e177a819e59776d7
shinbeomsik <tnghrltkwnst@naver.com>
2023-12-17 21:16:25 +0900

    12장 블로그 정리

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -27,15 +27,46 @@ public class ArticleApiController {
 	@Autowired
 	private ArticleService articleService; // 서비스 객체 주입
 
-	@GetMapping("/api/articles") 
+	@GetMapping("/api/articles")
 	public List<Article> index() {
 		return articleService.index();
 	}
 
 	@GetMapping("/api/articles/{id}")
-	public Article show(@PathVariable Long id) { 
+	public Article show(@PathVariable Long id) {
 		return articleService.show(id);
 	}
+
+	@PostMapping("/api/articles")
+	public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
+		Article created = articleService.create(dto);
+		return (created != null) ? ResponseEntity.status(HttpStatus.OK).body(created)
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+
+	@PatchMapping("/api/articles/{id}")
+	public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+		Article updated = articleService.update(id, dto);
+		return (updated != null) ? ResponseEntity.status(HttpStatus.OK).body(updated)
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+
+	@DeleteMapping("/api/articles/{id}")
+	public ResponseEntity<Article> delete(@PathVariable Long id) {
+		Article deleted = articleService.delete(id);
+		return (deleted != null) ? ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+
+	@PostMapping("/api/transaction-test") // 여러 게시글 생성 요청 접수
+	public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos) {
+		// transactionTest() 메서드 정의
+		List<Article> createdList = articleService.createArticles(dtos); // 서비스 호출
+		return (createdList != null) ? ResponseEntity.status(HttpStatus.OK).body(createdList)
+				: ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+	}
+
 }
 
 //서비스 하기전에 있던 코드들
